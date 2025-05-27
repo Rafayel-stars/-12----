4683a73cf8bb01d9392fc5f5d06d098e90ae12f9
Rafayel-stars <3077325915@qq.com>
2025-05-27 15:25:54 +0800

    Delete BackEnd directory

--- BackEnd/BackEnd/src/main/java/org/example/bookmanagesystem01/repository/LibrarySpaceRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.bookmanagesystem01.repository;
-
-import org.example.bookmanagesystem01.entity.LibrarySpace;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibrarySpaceRepository extends JpaRepository<LibrarySpace, Integer> {
-}
